aa6ae2211ff27b9e2561e5c3a9da9fbd394fa9a4
OubaallaElMehdi <smolville12@gmail.com>
2024-12-27 22:41:03 +0100

    add api prod

--- next-ace-front/src/services/AuthService.js
+++ next-ace-front/src/services/AuthService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_GATEWAY_URL || "http://localhost:8080";
+const BASE_URL = process.env.REACT_APP_GATEWAY_URL || "http://147.79.115.242:8080";
 
 const AuthService = {
   login: async (username, password) => {
